c80229b7601377f2eaae7cf43e63d11bc12678f7
shuricans <shuricans@gmail.com>
2022-09-18 12:59:21 +0300

    init backend-api-app module

---
+++ backend-api-app/src/main/java/no/war/habr/BackendApiAppApplication.java
@@ -0,0 +1,13 @@
+package no.war.habr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiAppApplication.class, args);
+    }
+
+}
