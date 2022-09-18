204458d12754013fa3caff4e71c0a77cac765ed8
shuricans <shuricans@gmail.com>
2022-09-18 13:07:55 +0300

    init picture-service module

---
+++ picture-service/src/main/java/no/war/habr/PictureServiceApplication.java
@@ -0,0 +1,13 @@
+package no.war.habr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureServiceApplication.class, args);
+    }
+
+}
