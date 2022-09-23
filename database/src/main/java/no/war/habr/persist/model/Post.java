a74baf7d2bd62ce5a7da2caa497819c922a5a984
shuricans <shuricans@gmail.com>
2022-09-23 16:58:58 +0300

    changing type "varchar" to "text" on "content" field

--- database/src/main/java/no/war/habr/persist/model/Post.java
+++ database/src/main/java/no/war/habr/persist/model/Post.java
@@ -34,7 +34,7 @@ public class Post extends BaseEntity {
     @Column(name = "title", nullable = false)
     private String title;
 
-    @Column(name = "content", nullable = false)
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(name = "description")
