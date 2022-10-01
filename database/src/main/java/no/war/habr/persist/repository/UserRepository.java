ff6bd0d144445d82df44e4b3317952fb63f8d733
shuricans <shuricans@gmail.com>
2022-10-01 19:46:20 +0300

    Added "findByUsername" method to the UserRepository. Tested it.

--- database/src/main/java/no/war/habr/persist/repository/UserRepository.java
+++ database/src/main/java/no/war/habr/persist/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package no.war.habr.persist.repository;
 
 import no.war.habr.persist.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+    @EntityGraph(attributePaths = {"roles"})
+    Optional<User> findByUsername(@Param("username") String username);
 }
---
+++ database/src/test/java/no/war/habr/persist/repository/UserRepositoryTest.java
@@ -0,0 +1,69 @@
+package no.war.habr.persist.repository;
+
+
+import no.war.habr.persist.model.ERole;
+import no.war.habr.persist.model.Role;
+import no.war.habr.persist.model.User;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@DisplayName("Tests for UserRepository")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@TestPropertySource(locations = "classpath:application-tc.properties")
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository underTest;
+    @Autowired
+    private RoleRepository roleRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        underTest.deleteAll();
+        roleRepository.deleteAll();
+    }
+
+    @Test
+    @DisplayName("findByUsername returns user when successful")
+    void shouldFindUserByUsernameAndFetchRoles() {
+        // given
+        // create role user
+        Role roleUser = Role.builder().name(ERole.ROLE_USER).build();
+        roleRepository.save(roleUser);
+        // create user
+        String username = "username";
+        User user = User.builder()
+                .username(username)
+                .firstName("firstName")
+                .password("password")
+                .roles(Set.of(roleUser))
+                .build();
+        underTest.save(user);
+
+        // when
+        Optional<User> optionalUser = underTest.findByUsername(username);
+
+        // then
+        assertThat(optionalUser).isPresent();
+
+        User userFound = optionalUser.get();
+
+        assertThat(userFound).isEqualTo(user);
+        assertThat(userFound.getUsername()).isEqualTo(username);
+        assertThat(userFound.getRoles()).hasSize(1).contains(roleUser);
+    }
+}
