ad30ed68d86d262e7f0beab9070d0745b6819dc7
shuricans <shuricans@gmail.com>
2022-10-18 13:26:50 +0300

    swagger, added default server url and context-path

--- backend-api-app/src/main/java/no/war/habr/config/DocumentationConfiguration.java
+++ backend-api-app/src/main/java/no/war/habr/config/DocumentationConfiguration.java
@@ -4,14 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(
-        title = "Habr",
-        description = "backend api",
-        version = "v1.0.0"
-))
+@OpenAPIDefinition(
+        info = @Info(
+            title = "Habr",
+            description = "backend api",
+            version = "v1.0.0"
+        ),
+        servers = {
+            @Server(url = "/api/v1", description = "Server URL")
+        })
 @SecurityScheme(
         name = "bearerAuth",
         type = SecuritySchemeType.HTTP,
