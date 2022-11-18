1f51f60afe1dcd2004c3b722ed3c8eb58d2616e5
shuricans <shuricans@gmail.com>
2022-11-18 16:43:06 +0300

    Task [B1] - liquibase migration, insert default topics

---
+++ database/src/main/resources/db/changelog/v-1.4/insert-topics.sql
@@ -0,0 +1,5 @@
+INSERT INTO topics (created, updated, name)
+VALUES (now(), now(), 'Дизайн'),
+       (now(), now(), 'Веб-разработка'),
+       (now(), now(), 'Мобильная разработка'),
+       (now(), now(), 'Маркетинг');
