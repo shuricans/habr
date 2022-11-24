6df6f9b90306efe406c8f5ef41d14a2d22a2d264
Ilmira <z.elle82@mail.ru>
2022-11-24 18:26:32 +0300

    Task[F2T7] - TopicController,TopicControllerTest

--- backend-api-app/src/main/java/no/war/habr/security/config/SecurityConfiguration.java
+++ backend-api-app/src/main/java/no/war/habr/security/config/SecurityConfiguration.java
@@ -59,6 +59,7 @@ public class SecurityConfiguration {
                         .antMatchers("/auth/logout").authenticated()
                         .antMatchers("/auth/**").permitAll()
                         .antMatchers("/posts", "/posts/{\\d+}").permitAll()
+                        .antMatchers("/topics").permitAll()
                         .antMatchers("/swagger-ui/**").permitAll()
                         .antMatchers("/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
