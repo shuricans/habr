539e0e483d27d5cd61c512cd7247b6990c3a49da
Ilmira <z.elle82@mail.ru>
2022-11-29 22:17:28 +0300

    Task[F7T1] - Marketing page

--- frontend-app/src/app/page/marketing-page/marketing-page.component.ts
+++ frontend-app/src/app/page/marketing-page/marketing-page.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Page} from "../../model/page";
+import {PageFilter} from "../../model/page-filter";
+import {PostFilter} from "../../model/post-filter";
+import {PostService} from "../../service/post.service";
+import {DataService} from "../../service/data.service";
 
 @Component({
   selector: 'app-marketing-page',
   templateUrl: './marketing-page.component.html',
   styleUrls: ['./marketing-page.component.scss']
 })
-export class MarketingPageComponent implements OnInit {
+export class MarketingPageComponent implements OnInit, OnDestroy {
+  page!: Page;
+  pageFilter!: PageFilter;
+  loading: boolean = true;
+  error: boolean = false;
+  postFilter: PostFilter;
+  private readonly MARKETING_TOPIC = 'Маркетинг';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private postService: PostService,
+              private dataService: DataService) {
+    this.postFilter = new PostFilter();
+    this.postFilter.topic = this.MARKETING_TOPIC;
   }
 
+  ngOnInit(): void {
+    this.pageFilter = this.dataService.getMarketingPageFilter();
+    this.getPage(this.pageFilter.page);
+  }
+
+  ngOnDestroy() {
+    this.dataService.setMarketingPageFilter(this.pageFilter);
+  }
+
+  changeSize(size: number) {
+    this.pageFilter.size = size;
+    this.getPage(1);
+  }
+
+  reloadPage() {
+    location.reload();
+  }
+
+  getPage(page: number) {
+    this.loading = true;
+    this.pageFilter.page = page;
+    this.dataService.setMarketingPageFilter(this.pageFilter);
+
+    this.postService.findAllPublishedPost(this.pageFilter, this.postFilter).subscribe({
+      next: page => {
+        this.page = page;
+        this.pageFilter.size = page.size;
+      },
+      error: err => {
+        console.error(`Error loading posts ${err}`);
+        this.error = true;
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
