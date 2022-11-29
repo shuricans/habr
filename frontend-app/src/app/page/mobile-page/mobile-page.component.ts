34db0b5fa2dc8fb0e63b29a97c4d2fd8a00230c2
Ilmira <z.elle82@mail.ru>
2022-11-29 19:02:14 +0300

    Task[F6T1] - Mobile page

--- frontend-app/src/app/page/mobile-page/mobile-page.component.ts
+++ frontend-app/src/app/page/mobile-page/mobile-page.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Page} from "../../model/page";
+import {PageFilter} from "../../model/page-filter";
+import {PostFilter} from "../../model/post-filter";
+import {PostService} from "../../service/post.service";
+import {DataService} from "../../service/data.service";
 
 @Component({
   selector: 'app-mobile-page',
   templateUrl: './mobile-page.component.html',
   styleUrls: ['./mobile-page.component.scss']
 })
-export class MobilePageComponent implements OnInit {
+export class MobilePageComponent implements OnInit, OnDestroy {
+  page!: Page;
+  pageFilter!: PageFilter;
+  loading: boolean = true;
+  error: boolean = false;
+  postFilter: PostFilter;
+  private readonly MOBILE_TOPIC = 'Мобильная разработка';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private postService: PostService,
+              private dataService: DataService) {
+    this.postFilter = new PostFilter();
+    this.postFilter.topic = this.MOBILE_TOPIC;
   }
 
+  ngOnInit(): void {
+    this.pageFilter = this.dataService.getMobilePageFilter();
+    this.getPage(this.pageFilter.page);
+  }
+
+  ngOnDestroy() {
+    this.dataService.setMobilePageFilter(this.pageFilter);
+  }
+
+  changeSize(size: number) {
+    this.pageFilter.size = size;
+    this.getPage(1);
+  }
+
+  reloadPage() {
+    location.reload();
+  }
+
+  getPage(page: number) {
+    this.loading = true;
+    this.pageFilter.page = page;
+    this.dataService.setMobilePageFilter(this.pageFilter);
+
+    this.postService.findAllPublishedPost(this.pageFilter, this.postFilter).subscribe({
+      next: page => {
+        this.page = page;
+        this.pageFilter.size = page.size;
+      },
+      error: err => {
+        console.error(`Error loading posts ${err}`);
+        this.error = true;
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+        window.scrollTo(0, 0);
+      }
+    });
+  }
 }
--- frontend-app/src/app/service/data.service.ts
+++ frontend-app/src/app/service/data.service.ts
@@ -13,6 +13,7 @@ export class DataService {
   private readonly LK_POST_FILTER = 'lk_posts_filter';
   private readonly DESIGN_PAGE_FILTER = 'design_page_filter';
   private readonly WEB_PAGE_FILTER = 'web_page_filter';
+  private readonly MOBILE_PAGE_FILTER = 'mobile_page_filter';
 
   private habrPageFilter!: PageFilter;
   private lkPostPageFilter!: PageFilter;
@@ -20,6 +21,7 @@ export class DataService {
   private lkPostFilter!: PostFilterOwn;
   private designPageFilter!: PageFilter;
   private webPageFilter!: PageFilter;
+  private mobilePageFilter!: PageFilter;
 
   public setHabrPageFilter(pagefilter: PageFilter) {
     this.habrPageFilter = pagefilter;
@@ -83,6 +85,8 @@ export class DataService {
     localStorage.removeItem(this.LK_ACTIVE_COMPONENT);
     localStorage.removeItem(this.LK_POST_FILTER);
     localStorage.removeItem(this.DESIGN_PAGE_FILTER);
+    localStorage.removeItem(this.WEB_PAGE_FILTER);
+    localStorage.removeItem(this.MOBILE_PAGE_FILTER);
   }
 
   getDesignPageFilter(): PageFilter {
@@ -111,5 +115,19 @@ export class DataService {
   setWebPageFilter(pageFilter: PageFilter) {
     this.webPageFilter = pageFilter;
     localStorage.setItem(this.WEB_PAGE_FILTER, JSON.stringify(this.webPageFilter));
+  }
+
+  getMobilePageFilter() {
+    if (localStorage.getItem(this.MOBILE_PAGE_FILTER)) {
+      this.mobilePageFilter = JSON.parse(localStorage.getItem(this.MOBILE_PAGE_FILTER)!);
+    } else {
+      this.mobilePageFilter = new PageFilter();
+    }
+    return this.mobilePageFilter;
+  }
+
+  setMobilePageFilter(pageFilter: PageFilter) {
+    this.mobilePageFilter = pageFilter;
+    localStorage.setItem(this.MOBILE_PAGE_FILTER, JSON.stringify(this.mobilePageFilter));
   }
 }
